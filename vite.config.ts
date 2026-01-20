9d6d241defaa9050ffbd3b534fe495ffac36b460
dinofilho <81691554+dinofilho@users.noreply.github.com>
2026-01-20 20:07:44 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": new URL("./src", import.meta.url).pathname,
-    },
-  },
 });
