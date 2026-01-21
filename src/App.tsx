64502299d4a9256cb3f9d69aa4523f906c599ba7
dinofilho <81691554+dinofilho@users.noreply.github.com>
2026-01-21 12:01:39 -0300

    Atualizar o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,12 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Landing */}
         <Route path="/" element={<Home />} />
 
-        {/* Auth */}
         <Route path="/auth" element={<Auth />} />
-
-        {/* App */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/companies" element={<Companies />} />
 
-        {/* Fallback */}
         <Route path="/404" element={<NotFound />} />
         <Route path="*" element={<Navigate to="/404" replace />} />
       </Routes>
