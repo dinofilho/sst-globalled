14cab53df5220565dc5bf763bcd14b0b35e0cf33
dinofilho <81691554+dinofilho@users.noreply.github.com>
2026-01-21 18:17:34 -0300

    Atualizar o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Companies from "./pages/Companies";
+import Employees from "./pages/Employees";
+import Dashboard from "./pages/Dashboard";
+import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
@@ -9,8 +12,14 @@ export default function App() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/companies" element={<Companies />} />
 
+        {/* Telas */}
+        <Route path="/companies" element={<Companies />} />
+        <Route path="/employees" element={<Employees />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/auth" element={<Auth />} />
+
+        {/* 404 */}
         <Route path="/404" element={<NotFound />} />
         <Route path="*" element={<Navigate to="/404" replace />} />
       </Routes>
