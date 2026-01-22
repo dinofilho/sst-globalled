2c35f674504fe0b6c4266ccb50416bf2b21c84e5
dinofilho <81691554+dinofilho@users.noreply.github.com>
2026-01-22 11:43:49 -0300

    Atualizar o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Companies from "./pages/Companies";
-import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
-        {/* FORÇADO: ao abrir o site, entra em Empresas */}
-        <Route path="/" element={<Companies />} />
+        {/* Página inicial */}
+        <Route path="/" element={<Home />} />
 
-        {/* Home fica em /home */}
-        <Route path="/home" element={<Home />} />
-
-        {/* Empresas também continua em /companies */}
+        {/* Cadastro de empresas */}
         <Route path="/companies" element={<Companies />} />
-
-        <Route path="/404" element={<NotFound />} />
-        <Route path="*" element={<Navigate to="/404" replace />} />
       </Routes>
     </HashRouter>
   );
